d8204fcd771923b0472fdb0f086e11cf886fcd05
Ravish Ailinani <ravish.ailinani@gmail.com>
2025-02-19 13:46:26 -0800

    Added age calculation feature

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
-print("Hello World")
-name = input ("whats your name?")
-print(f"Nice to meet you, {name}!")
+def greet_user():
+    print ("Hello World")
+    name = input ("whats your name?")
+    print(f"Nice to meet you, {name}!")
+    return name
+
+def calculate_age():
+    birth_year = int(input("What year were you born?"))
+    current_year = 2025
+    age = current_year - birth_year
+    return age
+
+def main():
+    user_name = greet_user()
+    user_age = calculate_age()
+    print (f"So, {name}, you are about {age} years old")
+    print ("Thanks for trying my program")
+
+    
